976d1799a1d0c1e66a8d5586d6a5033db491c878
Colin Ronayne <cronayne@gmail.com>
2018-12-17 12:52:16 +0100

    Added Two Way binding

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -22,6 +22,16 @@ class App extends Component {
     })
   }
 
+  nameChangedHandler = (event) => {
+    this.setState ({
+      persons: [
+        { name: "Bill", age: 22},
+        { name: event.target.value, age: 25},
+        { name: "Bat", age: 47}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -33,7 +43,8 @@ class App extends Component {
         <Person 
           name={this.state.persons[1].name} 
           age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, "Bill")}>My Hobbies: Racing</Person>
+          click={this.switchNameHandler.bind(this, "Bill")}
+          changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
         <Person 
           name={this.state.persons[2].name} 
           age={this.state.persons[2].age} />
